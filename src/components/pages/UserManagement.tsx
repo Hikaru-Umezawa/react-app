e2017cc41e3127ff1f11e47ecccff5acae1d0113
Hikaru-Umezawa <63339903+Hikaru-Umezawa@users.noreply.github.com>
2021-12-08 08:26:57 +0900

    Initial commit

---
+++ src/components/pages/UserManagement.tsx
@@ -0,0 +1,62 @@
+/* eslint-disabled react-hooks/exhaustive-deps */
+import { memo, useCallback, useEffect, VFC } from "react";
+import {
+  Center,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem
+} from "@chakra-ui/react";
+
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserCard } from "../organisms/user/userCard";
+import { UserDetailModal } from "../organisms/user/userDetailModal";
+import { useSelectUser } from "../../hooks/useSelectUser";
+import { useLoginUser } from "../../hooks/useLoginUser";
+
+export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { onSelectUser, selectedUser } = useSelectUser();
+  const { loginUser } = useLoginUser();
+
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users, onOpen });
+    },
+    [users]
+  );
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                id={user.id}
+                userName={user.username}
+                fullName={user.name}
+                onClick={onClickUser}
+              ></UserCard>
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
+    </>
+  );
+});
---
+++ src/router/Router.tsx
@@ -0,0 +1,38 @@
+import { memo, VFC } from "react";
+import { Route, Switch } from "react-router-dom";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+import { Login } from "../components/pages/Login";
+import { homeRoutes } from "../router/HomeRoutes";
+import { Page404 } from "../components/pages/Page404";
+import { LoginUserProvider } from "../providers/LoginUserProvider";
+
+export const Router: VFC = memo(() => {
+  return (
+    <Switch>
+      <LoginUserProvider>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route
+          path="/home"
+          render={({ match: { url } }) => (
+            <Switch>
+              {homeRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  <HeaderLayout>{route.children}</HeaderLayout>
+                </Route>
+              ))}
+            </Switch>
+          )}
+        />
+      </LoginUserProvider>
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  );
+});
